4a578e348122d7f9449337849668f4c9e8a101f5
Johann Barbie <johannbarbie@me.com>
2019-04-29 23:33:34 +0200

    fixed game condition to allow all results

--- contracts/GameCondition.sol
+++ contracts/GameCondition.sol
@@ -50,14 +50,19 @@ contract GameCondition {
   }
 
   // 1 house wins / 2 player wins / 0 equal
-  function runGame(uint256[] memory _handHouse, uint256[] memory _handPlayer) internal pure returns (uint) {
+  function runGame(uint256[] memory _handHouse, uint256[] memory _handPlayer) internal pure returns (uint256) {
     uint256 countHouse = 0;
     uint256 countPlayer = 0;
     for(uint i = 0; i < HAND_SIZE; i++) {
-      countHouse += (_handHouse[i + HAND_SIZE] > _handPlayer[i]) ? 1 : 0;
-      countPlayer += (_handHouse[i + HAND_SIZE] < _handPlayer[i]) ? 1 : 0;
+      countHouse = countHouse + ((_handHouse[i + HAND_SIZE] > _handPlayer[i]) ? 1 : 0);
+      countPlayer = countPlayer + ((_handPlayer[i] > _handHouse[i + HAND_SIZE]) ? 1 : 0);
     }
     return (countHouse > countPlayer) ? 1 : (countHouse < countPlayer) ? 2 : 0;
+  }
+
+  function _ecRecoverPersonal(bytes32 _cards, uint8 _v, bytes32 _r, bytes32 _s) internal pure returns (address) {
+    bytes32 sigHash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", _cards));
+    return ecrecover(sigHash, _v, _r, _s);
   }
 
   address constant TOKEN_ADDR = 0x1234111111111111111111111111111111111111;
@@ -67,30 +72,6 @@ contract GameCondition {
   address constant HOUSE = 0x3456333333333333333333333333333333333333;
   address constant PLAYER = 0x4567444444444444444444444444444444444444;
 
-  function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
-    if (_i == 0) {
-      return "0";
-    }
-    uint j = _i;
-    uint len;
-    while (j != 0) {
-      len++;
-      j /= 10;
-    }
-    bytes memory bstr = new bytes(len);
-    uint k = len - 1;
-    while (_i != 0) {
-      bstr[k--] = byte(uint8(48 + _i % 10));
-      _i /= 10;
-    }
-    return string(bstr);
-  }
-    
-  function _ecRecoverPersonal(bytes32 _cards, uint8 _v, bytes32 _r, bytes32 _s) internal pure returns (address) {
-    bytes32 sigHash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n", uint2str(32), _cards));
-    return ecrecover(sigHash, _v, _r, _s);
-  }
-
   function fulfill(bytes32 _cardsPlayer, bytes32 _r, bytes32 _s, uint8 _v) public {
     // check signature
     address signer = _ecRecoverPersonal(_cardsPlayer, _v, _r, _s);
@@ -99,24 +80,27 @@ contract GameCondition {
     // check cards
     uint256[] memory cards = readCards(CARD_BYTES);
     uint256[] memory cardsPlayer = readCards(_cardsPlayer);
+
+    // find winner
+    uint256 result = runGame(cards, cardsPlayer);
+
+    // check player hands match
     require(isCardsSame(cards, cardsPlayer), "player hands not same");
     
     // pull funds
     IERC20 token = IERC20(TOKEN_ADDR);
     uint256 balance = token.balanceOf(address(this));
-    token.transferFrom(PLAYER, address(this), balance / 5);
+    //token.transferFrom(PLAYER, address(this), balance / 5);
     balance = token.balanceOf(address(this));
-
-    // find winner
-    uint256 result = runGame(cards, cardsPlayer);
-    if (result == 1) {
-      token.transfer(HOUSE, balance);
-    } else if (result == 2) {
-      token.transfer(PLAYER, balance);
-    } else {
+    
+    if (result == 0) {
       token.transfer(HOUSE, (balance / 6) * 5);
       balance = token.balanceOf(address(this));
       token.transfer(PLAYER, balance);
+    } else if (result == 2) {
+      token.transfer(PLAYER, balance);
+    } else {
+      token.transfer(HOUSE, balance);
     }
   }
 }
--- test/gameCondition.js
+++ test/gameCondition.js
@@ -25,10 +25,18 @@ contract('GameCondition', (accounts) => {
   const player = '0xF3beAC30C498D9E26865F34fCAa57dBB935b0D74';
   const playerPriv = '0x278a5de700e29faae8e40e366ec5012b5ec63d36ec77e8a2417154cc1d25383f';
   let token;
-  let condition;
+  let originalByteCode;
 
-  beforeEach(async () => {
+  before(async () => {
     token = await SimpleToken.new();
+    originalByteCode = GameCondition._json.bytecode;
+  });
+
+  afterEach(() => {
+    GameCondition._json.bytecode = originalByteCode;
+  });
+
+  it('should allow player to win', async () => {
     const cards = '0x0000000000000000000000000105040603070208010901050206030704080409';
 
     // replace token address placeholder to real token address
@@ -43,15 +51,46 @@ contract('GameCondition', (accounts) => {
     tmp = replaceAll(tmp, '4567444444444444444444444444444444444444', player);
     GameCondition._json.bytecode = tmp;
 
-    condition = await GameCondition.new();
+    const condition = await GameCondition.new();
     // initialize contract
     await token.transfer(condition.address, 1000);
+    const permutation = '0x0000000000000000000000000000000000000000000002060307040801050409';
+    const hash = ethUtil.hashPersonalMessage(Buffer.from(permutation.replace('0x', ''), 'hex'));
+    const sig = ethUtil.ecsign(
+      hash,
+      Buffer.from(playerPriv.replace('0x', ''), 'hex'),
+    );
+    const tx = await condition.fulfill(permutation, `0x${sig.r.toString('hex')}`, `0x${sig.s.toString('hex')}`, sig.v).should.be.fulfilled;
+    // check transaction for events
+    assert.equal(tx.receipt.rawLogs[0].address, token.address);
+    assert.equal(tx.receipt.rawLogs.length, 1);
+    
+    assert.equal(tx.receipt.rawLogs[0].topics[1], '0x000000000000000000000000' + condition.address.replace('0x', '').toLowerCase());
+    assert.equal(tx.receipt.rawLogs[0].topics[2], '0x000000000000000000000000' + player.replace('0x', '').toLowerCase());
+    const remain = await token.balanceOf(condition.address);
+    assert.equal(remain.toNumber(), 0);
   });
 
-  it('should allow to have tied game', async () => {
-    const permutation = '0x0000000000000000000000000000000000000000000004090206030704080105';
-    const hash = Buffer.alloc(32);
-    Buffer.from(permutation.replace('0x', ''), 'hex').copy(hash);
+  it('should allow tied game', async () => {
+    const cards = '0x0000000000000000000000000105040603070208010901050406030702080109';
+
+    // replace token address placeholder to real token address
+    let tmp = GameCondition._json.bytecode;
+    // token address
+    tmp = replaceAll(tmp, '1234111111111111111111111111111111111111', token.address);
+    // cards
+    tmp = replaceAll(tmp, '2345222222222222222222222222222222222222222222222222222222222222', cards);
+    // house
+    tmp = replaceAll(tmp, '3456333333333333333333333333333333333333', house);
+    // player
+    tmp = replaceAll(tmp, '4567444444444444444444444444444444444444', player);
+    GameCondition._json.bytecode = tmp;
+
+    const condition = await GameCondition.new();
+    // initialize contract
+    await token.transfer(condition.address, 1000);
+    const permutation = '0x0000000000000000000000000000000000000000000001050406030702080109';
+    const hash = ethUtil.hashPersonalMessage(Buffer.from(permutation.replace('0x', ''), 'hex'));
     const sig = ethUtil.ecsign(
       hash,
       Buffer.from(playerPriv.replace('0x', ''), 'hex'),
@@ -61,7 +100,41 @@ contract('GameCondition', (accounts) => {
     assert.equal(tx.receipt.rawLogs[0].address, token.address);
     assert.equal(tx.receipt.rawLogs.length, 2);
     
-    // bytes32 anyone? :P
+    assert.equal(tx.receipt.rawLogs[0].topics[1], '0x000000000000000000000000' + condition.address.replace('0x', '').toLowerCase());
+    assert.equal(tx.receipt.rawLogs[0].topics[2], '0x000000000000000000000000' + house.replace('0x', '').toLowerCase());
+    const remain = await token.balanceOf(condition.address);
+    assert.equal(remain.toNumber(), 0);
+  });
+
+  it('should allow house to win', async () => {
+    const cards = '0x0000000000000000000000000105040603070208010901050206030701080109';
+
+    // replace token address placeholder to real token address
+    let tmp = GameCondition._json.bytecode;
+    // token address
+    tmp = replaceAll(tmp, '1234111111111111111111111111111111111111', token.address);
+    // cards
+    tmp = replaceAll(tmp, '2345222222222222222222222222222222222222222222222222222222222222', cards);
+    // house
+    tmp = replaceAll(tmp, '3456333333333333333333333333333333333333', house);
+    // player
+    tmp = replaceAll(tmp, '4567444444444444444444444444444444444444', player);
+    GameCondition._json.bytecode = tmp;
+
+    const condition = await GameCondition.new();
+    // initialize contract
+    await token.transfer(condition.address, 1000);
+    const permutation = '0x0000000000000000000000000000000000000000000002060307010801050109';
+    const hash = ethUtil.hashPersonalMessage(Buffer.from(permutation.replace('0x', ''), 'hex'));
+    const sig = ethUtil.ecsign(
+      hash,
+      Buffer.from(playerPriv.replace('0x', ''), 'hex'),
+    );
+    const tx = await condition.fulfill(permutation, `0x${sig.r.toString('hex')}`, `0x${sig.s.toString('hex')}`, sig.v).should.be.fulfilled;
+    // check transaction for events
+    assert.equal(tx.receipt.rawLogs[0].address, token.address);
+    assert.equal(tx.receipt.rawLogs.length, 1);
+    
     assert.equal(tx.receipt.rawLogs[0].topics[1], '0x000000000000000000000000' + condition.address.replace('0x', '').toLowerCase());
     assert.equal(tx.receipt.rawLogs[0].topics[2], '0x000000000000000000000000' + house.replace('0x', '').toLowerCase());
     const remain = await token.balanceOf(condition.address);
