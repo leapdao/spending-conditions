b0d932f4ec3d7ed7fa50831ffe8c397f29461017
Johann Barbie <johannbarbie@me.com>
2019-07-03 23:05:16 +0200

    some more functions

--- contracts/Air.sol
+++ contracts/Air.sol
@@ -4,41 +4,60 @@ import "./IERC1948.sol";
 import "openzeppelin-solidity/contracts/cryptography/ECDSA.sol";
 
 contract Air {
-    using ECDSA for bytes32;
+  using ECDSA for bytes32;
 
-    address constant CO2 = 0x1231111111111111111111111111111111111123;
-    address constant DAI = 0x2341111111111111111111111111111111111234;
-    // passports is an NFT contract, which holds a token for each participant
-    // of an event. The passport contains country the holder belongs to and the
-    // amount of CO2 released.
-    address constant PASSPORTS_ADDR = 0x3451111111111111111111111111111111111345;
+  address constant CO2 = 0x1231111111111111111111111111111111111123;
+  address constant DAI = 0x2341111111111111111111111111111111111234;
+  // passports is an NFT contract, which holds a token for each participant
+  // of an event. The passport contains country the holder belongs to and the
+  // amount of CO2 released.
+  address constant PASSPORTS_ADDR = 0x3451111111111111111111111111111111111345;
+  // CO2 flows from Earth to Air and maybe back. This is the address of the
+  // earth contract.
+  address constant EARTH_ADDR = 0x4561111111111111111111111111111111111456;
+  
+  function plantTree(uint256 amount, uint256 passport) public {
+
+    // signer information
+    IERC1948 passports = IERC1948(PASSPORTS_ADDR);
+    address signer = passports.ownerOf(passport);
+    address countryTreasury = address(bytes20(passports.readData(passport)));
+
+    // cash in payment
+    IERC20 dai = IERC20(DAI);
+    uint256 allowance = dai.allowance(signer, address(this));
+    require(allowance >= amount, "no funds allocated");
+    dai.transferFrom(signer, countryTreasury, amount);
     
-    function plantTree(
-      uint256 passport,
-      uint256 amount,
-      bytes memory sig,
-      address earthAddr
-    ) public {
-        // recover signatures
-        address signer = bytes32(amount).recover(sig);
-        
-        // verify with passports
-        IERC1948 passports = IERC1948(PASSPORTS_ADDR);
-        require(passports.ownerOf(passport) == signer, "signature invalid");
-        
-        // read the country's treasury address from the passport
-        address countryTreasury = address(bytes20(passports.readData(passport)));
+    // update passports
+    bytes32 data = passports.readData(passport);
+    // TODO: apply formula
+    passports.writeData(passport, bytes32(uint256(data) - amount));
 
-        // pay out trade        
-        IERC20 dai = IERC20(DAI);
-        dai.transferFrom(signer, countryTreasury, amount);
-        
-        // update passports
-        bytes32 data = passports.readData(passport);
-        passports.writeData(passport, bytes32(uint256(data) - amount));
+    // lock CO2
+    IERC20 co2 = IERC20(CO2);
+    co2.transfer(EARTH_ADDR, amount);
+  }
 
-        // lock CO2
-        IERC20 co2 = IERC20(CO2);
-        co2.transfer(earthAddr, amount);
-    }
+  // account used as game master.
+  address constant PRIME_MOTHER = 0x5671111111111111111111111111111111111567;
+
+  // used to model natural reduction of CO2 if below run-away point
+  function lockCO2(uint256 amount, bytes memory sig) public {
+    address signer = bytes32(bytes20(address(this))).recover(sig);
+    require(signer == PRIME_MOTHER, "signer does not match");
+    // lock CO2
+    IERC20 co2 = IERC20(CO2);
+    co2.transfer(EARTH_ADDR, amount);
+  }
+
+  // used to combine multiple contract UTXOs into one.
+  function consolidate(bytes memory sig) public {
+    address signer = bytes32(bytes20(address(this))).recover(sig);
+    require(signer == PRIME_MOTHER, "signer does not match");
+    // lock CO2
+    IERC20 co2 = IERC20(CO2);
+    uint256 amount = co2.balanceOf(address(this));
+    co2.transfer(EARTH_ADDR, amount);
+  }
 }
--- contracts/Earth.sol
+++ contracts/Earth.sol
@@ -4,47 +4,61 @@ import "./IERC1948.sol";
 import "openzeppelin-solidity/contracts/cryptography/ECDSA.sol";
 
 contract Earth {
-    using ECDSA for bytes32;
+  using ECDSA for bytes32;
 
-    address constant CO2 = 0x1231111111111111111111111111111111111123;
-    address constant DAI = 0x2341111111111111111111111111111111111234;
-    // passports is an NFT contract, which holds a token for each participant
-    // of an event. The passport contains country the holder belongs to and the
-    // amount of CO2 released.
-    address constant PASSPORTS_ADDR = 0x3451111111111111111111111111111111111345;
-    // CO2 flows from Earth to Air and maybe back. This is the address of the
-    // air contract.
-    address constant AIR_ADDR = 0x4561111111111111111111111111111111111456;
+  address constant CO2 = 0x1231111111111111111111111111111111111123;
+  address constant DAI = 0x2341111111111111111111111111111111111234;
+  // passports is an NFT contract, which holds a token for each participant
+  // of an event. The passport contains country the holder belongs to and the
+  // amount of CO2 released.
+  address constant PASSPORTS_ADDR = 0x3451111111111111111111111111111111111345;
+  
+  function trade(
+    uint256 passportA, uint256 passportB,
+    uint256 factorA, uint256 factorB,
+    bytes memory sigA, bytes memory sigB,
+    address airAddr
+  ) public {
+    // recover signatures
+    address signerA = bytes32(factorA).recover(sigA);
+    address signerB = bytes32(factorB).recover(sigB);
     
-    function trade(
-      uint256 passportA, uint256 passportB,
-      uint256 factorA, uint256 factorB,
-      bytes memory sigA, bytes memory sigB
-    ) public {
-        // recover signatures
-        address signerA = bytes32(factorA).recover(sigA);
-        address signerB = bytes32(factorB).recover(sigB);
-        
-        // verify with passports
-        IERC1948 passports = IERC1948(PASSPORTS_ADDR);
-        require(passports.ownerOf(passportA) == signerA, "signature A invalid");
-        require(passports.ownerOf(passportB) == signerB, "signature B invalid");
+    // verify with passports
+    IERC1948 passports = IERC1948(PASSPORTS_ADDR);
+    require(passports.ownerOf(passportA) == signerA, "signature A invalid");
+    require(passports.ownerOf(passportB) == signerB, "signature B invalid");
 
-        // pay out trade        
-        IERC20 dai = IERC20(DAI);
-        dai.transfer(signerA, factorA);
-        dai.transfer(signerB, factorB);
-        
-        // update passports
-        bytes32 dataA = passports.readData(passportA);
-        passports.writeData(passportA, bytes32(uint256(dataA) + factorA));
-        bytes32 dataB = passports.readData(passportB);
-        passports.writeData(passportB, bytes32(uint256(dataB) + factorB));
+    // pay out trade        
+    IERC20 dai = IERC20(DAI);
+    // TODO: apply formula
+    dai.transfer(signerA, factorA);
+    dai.transfer(signerB, factorB);
+    
+    // update passports
+    bytes32 dataA = passports.readData(passportA);
+    // TODO: apply formula
+    passports.writeData(passportA, bytes32(uint256(dataA) + factorA));
+    bytes32 dataB = passports.readData(passportB);
+    passports.writeData(passportB, bytes32(uint256(dataB) + factorB));
 
-        // emit CO2
-        if (factorA > 100 || factorB > 100) {
-            IERC20 co2 = IERC20(CO2);
-            co2.transfer(AIR_ADDR, factorA + factorB);
-        }
+    // emit CO2
+    if (factorA > 100 || factorB > 100) {
+      IERC20 co2 = IERC20(CO2);
+      // TODO: apply formula
+      co2.transfer(airAddr, factorA + factorB);
     }
+  }
+
+  // account used as game master.
+  address constant PRIME_MOTHER = 0x5671111111111111111111111111111111111567;
+
+  // used to model natural increase of CO2 if above run-away point.
+  // question: temprature will increase, but will CO2 increase as well?
+  function unlockCO2(uint256 amount, address airAddr, bytes memory sig) public {
+    address signer = bytes32(bytes20(address(this))).recover(sig);
+    require(signer == PRIME_MOTHER, "signer does not match");
+    // unlock CO2
+    IERC20 co2 = IERC20(CO2);
+    co2.transfer(airAddr, amount);
+  }
 }
